ed4772ca22159b4668533b52c03c01acc00c5975
Stanislav Vasilchenko <stanilav.vasilchenko@yandex.ru>
2023-11-07 22:00:54 +0300

    added post request

--- catalog/views.py
+++ catalog/views.py
@@ -6,4 +6,9 @@ def home(request):
 
 
 def contacts(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        phone = request.POST.get("phone")
+        message = request.POST.get("message")
+        print(f"От {name} ({phone}) получено сообщение: {message}")
     return render(request, "main/contacts.html")
