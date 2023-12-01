5feea746f1ada082b0693161778f7be6c33b1799
Stanislav Vasilchenko <stanilav.vasilchenko@yandex.ru>
2023-12-02 00:10:35 +0300

    add BlogView, BlogListView

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from blog.apps import BlogConfig
-from blog.views import BlogView, BlogCreateView
+from blog.views import BlogCreateView, BlogView, BlogListView
 
 app_name = BlogConfig.name
 
 urlpatterns = [
     path('', BlogView.as_view(), name='home'),
     path('create/', BlogCreateView.as_view(), name='create'),
+    path('articles/', BlogListView.as_view(), name='articles'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, CreateView
+from django.views.generic import TemplateView, CreateView, ListView
 from blog.models import Blog
 
 
 class BlogView(TemplateView):
     template_name = 'blog/index_blog.html'
 
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['object_list'] = Blog.objects.all()[:4]
+        return context_data
+
+
+class BlogListView(ListView):
+    model = Blog
+
 
 class BlogCreateView(CreateView):
     model = Blog
     fields = ('blog_title', 'blog_content', 'blog_image', 'publication_sign')
     success_url = reverse_lazy('blog:home')
-
-
-
-
