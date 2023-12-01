ee1152d3ecf784bf44833a8bfc743c4c0d758fc7
Stanislav Vasilchenko <stanilav.vasilchenko@yandex.ru>
2023-12-01 23:23:01 +0300

    add BlogCreateView

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from blog.apps import BlogConfig
-
+from blog.views import BlogView, BlogCreateView
 
 app_name = BlogConfig.name
 
 urlpatterns = [
-    # path('', ProductView.as_view(), name='home'),
+    path('', BlogView.as_view(), name='home'),
+    path('create/', BlogCreateView.as_view(), name='create'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, CreateView
+from blog.models import Blog
 
-# Create your views here.
+
+class BlogView(TemplateView):
+    template_name = 'blog/index_blog.html'
+
+
+class BlogCreateView(CreateView):
+    model = Blog
+    fields = ('blog_title', 'blog_content', 'blog_image', 'publication_sign')
+    success_url = reverse_lazy('blog:home')
+
+
+
+
