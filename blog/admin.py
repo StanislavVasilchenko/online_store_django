eb546d93500a36be4dc78da033fac1d7c1945487
Stanislav Vasilchenko <stanilav.vasilchenko@yandex.ru>
2023-12-01 22:01:52 +0300

    register blog admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Blog
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('blog_title', 'blog_content', 'date_of_creation', 'publication_sign', 'view_count')
+    list_filter = ('publication_sign',)
