38892caa764072132054fdc602103bf41875f572
Stanislav Vasilchenko <stanilav.vasilchenko@yandex.ru>
2023-12-01 21:00:45 +0300

    create ProductsDetailView

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import contacts, one_product, ProductView, ProductsListView
+from catalog.views import contacts, ProductView, ProductsListView, ProductsDetailView
 
 app_name = CatalogConfig.name
 
@@ -9,6 +9,6 @@ urlpatterns = [
     path('', ProductView.as_view(), name='home'),
     path('contacts/', contacts, name='contacts'),
     path('products/', ProductsListView.as_view(), name='products'),
-    path('one_product/<int:pk>', one_product, name='one_product'),
+    path('one_product/<int:pk>', ProductsDetailView.as_view(), name='one_product'),
 
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, TemplateView
+from django.views.generic import ListView, TemplateView, DetailView
 
 from catalog.models import Product
 
@@ -14,11 +14,13 @@ class ProductView(TemplateView):
         return context_data
 
 
-# def home(request):
-#     context = {
-#         'product_list': Product.objects.order_by('date_of_creation')[:4]
-#     }
-#     return render(request, "catalog/index.html", context)
+class ProductsListView(ListView):
+    model = Product
+
+
+class ProductsDetailView(DetailView):
+    model = Product
+    template_name = 'catalog/product_detail.html'
 
 
 def contacts(request):
@@ -28,26 +30,3 @@ def contacts(request):
         message = request.POST.get("message")
         print(f"От {name} ({phone}) получено сообщение: {message}")
     return render(request, "catalog/contacts.html")
-
-
-class ProductsListView(ListView):
-    model = Product
-    #
-    # def get_context_data(self, *args, **kwargs):
-    #     context_data = super().get_context_data(*args, **kwargs)
-    #     context_data['object_list'] = Product.objects.all()[:4]
-    #     return context_data
-
-
-# def products(request):
-#     context = {
-#         'product_list': Product.objects.all()
-#     }
-#     return render(request, "catalog/product_list.html", context)
-
-
-def one_product(request, pk):
-    context = {
-        'product_list': Product.objects.filter(id=pk)
-    }
-    return render(request, "catalog/one_product.html", context)
