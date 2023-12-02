8b3621b23e0503c6c68c037103e449102a4351b2
Stanislav Vasilchenko <stanilav.vasilchenko@yandex.ru>
2023-12-02 14:09:11 +0300

    loading an image from the form

--- blog/views.py
+++ blog/views.py
@@ -30,7 +30,6 @@ class BlogDetailsView(DetailView):
 class BlogUpdateView(UpdateView):
     model = Blog
     fields = ('blog_title', 'blog_content', 'blog_image', 'publication_sign')
-    success_url = reverse_lazy('blog:home')
 
     def get_success_url(self):
         return reverse('blog:blog_detail', args=[self.object.id])
