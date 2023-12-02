9df3f2c79af7bdcae547739968d0b826d0914c08
Stanislav Vasilchenko <stanilav.vasilchenko@yandex.ru>
2023-12-02 13:23:32 +0300

    add BlogUpdateView

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from blog.apps import BlogConfig
-from blog.views import BlogCreateView, BlogView, BlogListView, BlogDetailsView
+from blog.views import BlogCreateView, BlogView, BlogListView, BlogDetailsView, BlogUpdateView
 
 app_name = BlogConfig.name
 
@@ -10,4 +10,5 @@ urlpatterns = [
     path('create/', BlogCreateView.as_view(), name='create'),
     path('articles/', BlogListView.as_view(), name='articles'),
     path('detail/<int:pk>', BlogDetailsView.as_view(), name='blog_detail'),
+    path('update/<int:pk>', BlogUpdateView.as_view(), name='blog_update'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views.generic import TemplateView, CreateView, ListView, DetailView
+from django.urls import reverse_lazy, reverse
+from django.views.generic import TemplateView, CreateView, ListView, DetailView, UpdateView
 from blog.models import Blog
 
 
@@ -26,3 +26,11 @@ class BlogCreateView(CreateView):
 class BlogDetailsView(DetailView):
     model = Blog
 
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    fields = ('blog_title', 'blog_content', 'blog_image', 'publication_sign')
+    success_url = reverse_lazy('blog:home')
+
+    def get_success_url(self):
+        return reverse('blog:blog_detail', args=[self.object.id])
