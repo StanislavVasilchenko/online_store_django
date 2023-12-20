4e1045a0466d2bb9adce665ea8bb6d8448428ffb
Stanislav Vasilchenko <stanilav.vasilchenko@yandex.ru>
2023-12-20 21:54:52 +0300

    add clean_product_name

--- catalog/forms.py
+++ catalog/forms.py
@@ -4,7 +4,13 @@ from catalog.models import Product
 
 
 class ProductForm(forms.ModelForm):
-
     class Meta:
         model = Product
         fields = '__all__'
+
+    def clean_product_name(self):
+        cleaned_data = self.cleaned_data['product_name']
+        stop_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
+        if cleaned_data in stop_words:
+            raise forms.ValidationError('Вы использовали запрещенное слово в наименовании продукта')
+        return cleaned_data
