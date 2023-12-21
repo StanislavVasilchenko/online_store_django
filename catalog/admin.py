ba09fc51735efb2a8f1707b702cabdc15915efcf
Stanislav Vasilchenko <stanilav.vasilchenko@yandex.ru>
2023-12-21 20:38:54 +0300

    add model Version

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from catalog.models import Product, Category
+from catalog.models import Product, Category, Version
 
 
 @admin.register(Product)
@@ -13,3 +13,9 @@ class ProductAdmin(admin.ModelAdmin):
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'category_name', 'category_description')
+
+
+@admin.register(Version)
+class VersionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'product', 'number', 'is_activ',)
+
--- catalog/models.py
+++ catalog/models.py
@@ -25,8 +25,22 @@ class Category(models.Model):
     category_description = models.TextField(verbose_name='Описание')
 
     def __str__(self):
-        return f'{self.id} {self.category_name}'
+        return f'{self.category_name}'
 
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
+
+
+class Version(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='Продукт')
+    number = models.SmallIntegerField(verbose_name='Версия')
+    name = models.CharField(max_length=250, verbose_name='Название версии')
+    is_activ = models.BooleanField(verbose_name='признак текущей версии')
+
+    def __str__(self):
+        return f'{self.product} - {self.number} / {self.is_activ}'
+
+    class Meta:
+        verbose_name = 'Версия'
+        verbose_name_plural = 'Версии'
