3d8a6be068c4983729ff20953584555bb4d334e2
Stanislav Vasilchenko <stanilav.vasilchenko@yandex.ru>
2023-12-23 12:31:34 +0300

    add clean_name in VersionAdmin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
+from catalog.forms import ProductForm, VersionForm
 from catalog.models import Product, Category, Version
 
 
@@ -8,6 +9,7 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'product_name', 'price', 'category',)
     list_filter = ('category',)
     search_fields = ('product_name', 'description')
+    form = ProductForm
 
 
 @admin.register(Category)
@@ -18,4 +20,5 @@ class CategoryAdmin(admin.ModelAdmin):
 @admin.register(Version)
 class VersionAdmin(admin.ModelAdmin):
     list_display = ('name', 'product', 'number', 'is_activ',)
+    form = VersionForm
 
