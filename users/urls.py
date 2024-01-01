a42024386815c1d195b0bdbc5b836aae4f69de26
stanislav <stanislav.vasilchenko@yandex.ru>
2024-01-02 01:38:45 +0300

    add pass recovery url

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import RegisterView, VerificationTemplateView
+from users.views import RegisterView, VerificationTemplateView, PasswordRecoveryView
 
 app_name = UsersConfig.name
 
@@ -11,4 +11,5 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
     path('verify/', VerificationTemplateView.as_view(), name='verify'),
+    path('pass_recovery/', PasswordRecoveryView.as_view(), name='pass_recovery'),
 ]
